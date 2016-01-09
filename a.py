063d337b6ad25a346e5457f657a43f0775ce4325
TM <none@none>
2016-01-09 23:51:43 +0100

    word distances work

--- a.py
+++ a.py
@@ -1,8 +1,39 @@
 #!/usr/bin/python3
 
+import collections
+
+class Candidate:
+    def __init__(self, doc_id, word_occurrences):
+        self.doc_id = doc_id
+        self.last_occurrences = word_occurrences
+        self.min_dist = 0
+
+    def __repr__(self):
+        return "Candidate({}, {}, {})".format(self.doc_id, self.min_dist, self.last_occurrences)
+
+def groupped_occurrences(occurrences):
+    d = collections.defaultdict(list)
+    for word, pos in occurrences:
+        d[word].append(pos)
+    return d
+
+def min_dist(xpositions, ypositions):
+    m = 1000
+    for x in xpositions:
+        for y in ypositions:
+            if x < y:
+                if y - x - 1 < m:
+                    m = y - x - 1
+            else:
+                if x - y < m:
+                    m = x - y
+            if m <= 0:
+                return 0
+    return m
+
 class I:
     def __init__(self):
-        self._index = {}
+        self._index = collections.defaultdict(list)
 
     def analyse(self, record):
         words = record.split(" ")
@@ -10,49 +41,51 @@ class I:
 
     def index(self, records):
         for doc_id, record in enumerate(records):
-            words = self.analyse(record)
-            for word, pos in words:
-                if word not in self._index:
-                    self._index[word] = []
-                self._index[word].append((doc_id, pos))
+            occurrences = self.analyse(record)
+            for word, positions in groupped_occurrences(occurrences).items():
+                self._index[word].append((doc_id, sorted(positions)))
 
     def find_one(self, word):
         docs = self._index.get(word, [])
-        return [doc_id for doc_id, pos in docs]
+        return [Candidate(doc_id, positions) for doc_id, positions in docs]
 
     def find(self, query):
-        words = self.analyse(query)
-        if not words:
+        occurrences = self.analyse(query)
+        if not occurrences:
             return []
 
-        word, pos = words[0]
-        docs = self.find_one(word)
-        for word, pos in words[1:]:
-            new_docs = self.find_one(word)
-            docs = self.merge(docs, new_docs)
+        word, _ = occurrences[0]
+        candidates = self.find_one(word)
+        for word, pos in occurrences[1:]:
+            new_candidates = self.find_one(word)
+            candidates = self.merge(candidates, new_candidates)
 
-        return docs
+        return candidates
 
     def merge(self, xs, ys):
-        ds = []
+        cs = []
         ix = iy = 0
+        print("merging", xs, ys)
         while ix < len(xs) and iy < len(ys):
-            doc_x = xs[ix]
-            doc_y = ys[iy]
-            if doc_x < doc_y:
+            cndx, cndy = xs[ix], ys[iy]
+            if cndx.doc_id < cndy.doc_id:
                 ix += 1
-            elif doc_x > doc_y:
+            elif cndx.doc_id > cndy.doc_id:
                 iy += 1
             else:
-                ds.append(doc_x)
+                xpositions = cndx.last_occurrences
+                ypositions = cndy.last_occurrences
+                c = Candidate(cndx.doc_id, ypositions)
+                c.min_dist = cndx.min_dist + min_dist(xpositions, ypositions)
+                cs.append(c)
                 ix, iy = ix + 1, iy + 1
-        return ds
+
+        return cs
 
     def translate_docs(self, result, records):
         return [
-            records[doc_id]
-            for doc_id
-            in result
+            (records[cnd.doc_id], cnd.min_dist)
+            for cnd in result
         ]
 
 
@@ -73,10 +106,10 @@ if __name__ == "__main__":
         "taky auto jelo srat",
         "neslo se vysrat mimo silnici",
         "tak to v seste hodine taky",
-        "sestou hodinu se vysralo tezce",
+        "seste hodine se vysralo tezce",
     ]
 
     x = I()
     x.index(records)
-    y = x.find("po auto kravate")
+    y = x.find("hodine seste")
     print("found:", x.translate_docs(y, records))
